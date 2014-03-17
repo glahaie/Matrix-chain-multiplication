ba4ace32a3f636e3ecd0788fc93114ff2f3464a5
Guillaume Lahaie <glahaie@gmail.com>
2014-03-17 17:23:07 -0400

    Ajout des fichiers de base

---
+++ matrices.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+# -*- coding:utf8 -*-
+
+"""matrices.py
+   Par Guillaume Lahaie
+   dernière modification: 17 mars 2014
+
+   Implémentation des algorithmes de parenthésage vu en cours.
+   Tout d'abord, l'algorithme diviser pour régner, et ensuite
+   l'algorithme de programmation dynamique
+"""
+
+from numpy import matlib
+
+
+def multiplication(a, b):
+    """Multiplie les matrices a et b, et retourne
+       le résultat dans une nouvelle matrice"""
+
+
+
+
